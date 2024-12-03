4fa2629a413432453b67c02cf62dc55ee8f196c4
Quoc Nguyen <quocng777@gmail.com>
2024-12-03 17:58:07 +0700

    chore: remove nest default files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { getTypeOrmModuleOptions } from './config/typeorm.config';
@@ -26,7 +24,5 @@ import { DevtoolsModule } from '@nestjs/devtools-integration';
     UserModule,
     AuthModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
