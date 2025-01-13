8455bce98bb62c7b0aa89037a067e9af483992f0
Quoc Nguyen <quocng777@gmail.com>
2025-01-13 22:10:57 +0700

    feat: finish ai navigation

--- src/modules/ai/ai.module.ts
+++ src/modules/ai/ai.module.ts
@@ -3,12 +3,15 @@ import { AiController } from "./ai.controller";
 import { AiService } from "./ai.service";
 import { HttpModule } from "@nestjs/axios";
 import { LlmHttpConfigService } from "@/config/llm-http.config";
+import { MongooseModule } from "@nestjs/mongoose";
+import { MovieModel, MovieSchema } from "../movies/schemas/movie.schema";
 
 @Module({
   imports: [
     HttpModule.registerAsync({
       useClass: LlmHttpConfigService,
-    })
+    }),
+    MongooseModule.forFeature([{schema: MovieSchema, name: "movies"}])
   ],
   controllers: [AiController],
   providers: [AiService]
