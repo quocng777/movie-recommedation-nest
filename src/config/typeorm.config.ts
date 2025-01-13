ae09e0a98c90469e615b2487506fdb589feeebae
21120346 <111715558+TrietTruong2243@users.noreply.github.com>
2025-01-13 20:31:47 +0700

    fix: add entity to typeorm config

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -8,6 +8,8 @@ import PlaylistItem from "@/modules/playlist/entities/playlist-item.entity";
 import Rating from "@/modules/movies/entities/rating.entity";
 import Review from "@/modules/movies/entities/review.entity";
 import { Movie } from "@/modules/movies/entities/movie.entity";
+import { Person } from "@/modules/person/entities/person.entity";
+import { Movies_Casts_Combined } from "@/modules/movies/entities/movie-cast-combined.entity";
 
 const nodeEnv = process.env.NODE_ENV || 'development';
 
@@ -18,7 +20,7 @@ dotenvConfig(
 );
 
 export const dataSourceOptions: DataSourceOptions = {
-    synchronize: true,
+    synchronize: false,
     type: 'postgres',
     host: process.env.DB_HOST,
     username: process.env.DB_USERNAME,
@@ -26,7 +28,7 @@ export const dataSourceOptions: DataSourceOptions = {
     database: process.env.DB_DATABASE,
     port: Number.parseInt(process.env.DB_PORT),
     migrationsTableName: '_migrations',
-    entities: [User, LikedMovie, WatchLater, Playlist, PlaylistItem, Rating, Review, Movie],
+    entities: [User, LikedMovie, WatchLater, Playlist, PlaylistItem, Rating, Review, Movie, Person, Movies_Casts_Combined],
     migrations: ['dist/migrations/*{.ts,.js}'], //problem: when you want to run migration please change the path to 'src/migration/*{.ts,.js}''. I'm trying to fix it, but not now
     migrationsRun: false,
 };
